d7dac7d90d768110b95ea4408db4c0bb614a103e
Kirmct <kirmctneto@gmail.com>
2025-01-31 15:57:18 -0400

    platform microservice completed

---
+++ PlatformService/Program.cs
@@ -0,0 +1,42 @@
+using Microsoft.EntityFrameworkCore;
+using PlatformService.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Add database
+builder.Services.AddDbContext<AppDbContext>(opt =>
+    opt.UseInMemoryDatabase("InMen"));
+
+//Add repository
+builder.Services.AddScoped<IPlatformRepository, PlatformRepository>();
+
+builder.Services.AddControllers();
+
+// Add Automapper
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+//swagger
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+// app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+//populating with static class
+PrepDb.PrepPopulation(app);
+
+Console.WriteLine("--> Applications is running");
+app.Run();
